c69ecab7adcd39e7b45c86e285745a938fa80bae
DaisyDelgadillo <daisydelgadillo97@gmail.com>
2020-11-13 03:00:30 -0800

    Added buttons to HTML & JS ,input missing emoji

--- assets/js/button.js
+++ assets/js/button.js
@@ -7,13 +7,15 @@ let clickSimple = document.getElementById('simple');
 let clickWifi = document.getElementById('wifi');
 let clickSocials = document.getElementById('socials');
 let clickImpossible = document.getElementById('impossible');
+let clickBank = document.getElementById('bank');
+
+
 
 // stores the fetched data in array
 let wordBank = [];
 let nounBank = [];
 let adjBank = [];
 let verbBank = [];
-
 
 // randomWord Button
 clickW.addEventListener('click', function () {
